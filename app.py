da79d5e89fa0a7d9e0bd52171b1deccff17a5dd3
Delta Student <yashrajmohite07@gmail.com>
2026-08-17 22:54:22 +0530

    Fix app syntax error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.screens.student_screen import student_screen
 from src.components.dialog_auto_enroll import auto_enroll_dialog
 def main():
     st.set_page_config(
-        page_title='SnapClass - Making Attendance Faster Using AI'
+        page_title='SnapClass - Making Attendance Faster Using AI',
         page_icon="https://i.ibb.co/YTYGn5qv/logo.png"
     )
     if "login_type" not in st.session_state:
